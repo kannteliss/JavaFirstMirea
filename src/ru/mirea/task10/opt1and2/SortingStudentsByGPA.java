67970f85fe5efae0a000106afb79f6082d224893
Anna Znaydenova <znaydann@gmail.com>
2024-10-04 00:07:17 +0300

    added new tasks 25-32

--- src/ru/mirea/task10/opt1and2/SortingStudentsByGPA.java
+++ src/ru/mirea/task10/opt1and2/SortingStudentsByGPA.java
@@ -6,45 +6,6 @@ import java.util.Comparator;
 import java.util.List;
 
 public class SortingStudentsByGPA {
-
-//    private List<Student> students;
-//
-//    public SortingStudentsByGPA() {
-//        students = new ArrayList<>();
-//    }
-//
-//    public void addStudent(Student student) {
-//        students.add(student);
-//    }
-//
-//    public void sortStudentsByGPA() {
-//        Collections.sort(students, new Comparator<Student>() {
-//            @Override
-//            public int compare(Student s1, Student s2) {
-//                return Double.compare(s2.getGPA(), s1.getGPA());
-//            }
-//        });
-//    }
-//
-//    public void printSortedStudents() {
-//        System.out.println("Отсортированный список студентов по GPA:");
-//        for (Student student : students) {
-//            System.out.println(student);
-//        }
-//    }
-//
-//    // Метод для сортировки по имени - убывание
-//    public void sortByName() {
-//        Collections.sort(students, (s1, s2) -> s2.getName().compareTo(s1.getName()));
-//    }
-//
-//    // Метод для вывода списка студентов
-//    public void outArray() {
-//        System.out.println("Список студентов:");
-//        for (Student student : students) {
-//            System.out.println(student);
-//        }
-//    }
 private List<Student> students;
 
     public SortingStudentsByGPA() {
